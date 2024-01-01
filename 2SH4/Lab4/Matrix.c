de6b22135c02ecb82b8e81fcc2cbac8ca670942c
Vraj Patel <72108286+vraj26@users.noreply.github.com>
2024-01-01 10:58:42 -0500

    Update Matrix.c

--- 2SH4/Lab4/Matrix.c
+++ 2SH4/Lab4/Matrix.c
@@ -1,1 +1,48 @@
 
+#include <string>
+using namespace std;
+
+class Matrix {
+
+	public:
+
+		// Default Constructor
+		Matrix();		
+
+		// Additional Constructors
+		Matrix( int row, int col );
+		Matrix( int row, int col, int **table );
+
+		//Copr COnstructor for deep copy??
+		Matrix( const Matrix &m);
+
+		// Do you think you need a Destructor?  Why or why not?
+		~Matrix();
+		
+
+		// Getters
+		int getElement(int i, int j);
+		int getsizeofrows();
+		int getsizeofcols();
+
+		// Setters
+		bool setElement(int x, int i, int j);
+
+		// Other Member Functions
+		Matrix copy();						// Create a Copy of This Matrix
+		void addTo( Matrix &m );				// Add Matrix m to this matrix
+		Matrix subMatrix(int i, int j);     // Obtain the upper-left submatrix of this matrix
+		string toString();					// String Conversion Method
+
+
+	private:
+
+		// Stack Data Members
+		int    rowsNum;
+		int    colsNum;
+
+		// Heap Data Members - Do you need to release them?  Where, when, and how?
+		int    **matrixData;
+
+};
+
