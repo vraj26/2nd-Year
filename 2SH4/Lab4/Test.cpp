1ef02d1c17b487a20efa4e6c2a166d12ce9d9e1b
Vraj Patel <72108286+vraj26@users.noreply.github.com>
2024-01-01 10:59:24 -0500

    Add files via upload

---
+++ 2SH4/Lab4/Test.cpp
@@ -0,0 +1,500 @@
+#include "./cute/cute.h"
+#include "./cute/ide_listener.h"
+#include "./cute/xml_listener.h"
+#include "./cute/cute_runner.h"
+#include "Matrix.h"
+#include <iostream>
+
+
+void testConstructor1Invalid1() {
+		// for constructor1
+		int invalid_row1 = -1, invalid_col1 = -2;
+
+		string row1="0 0 0 \n", row2="0 0 0 \n",row3="0 0 0 \n";
+		string expected = row1 + row2 + row3;
+   	 	// test constructor1; invalid inputs
+		Matrix a = Matrix(invalid_row1,invalid_col1);
+
+		ASSERT_EQUAL(expected,a.toString());
+	}
+
+
+
+void testConstructor1Invalid2() {
+		// for constructor1
+		int invalid_row2 = 1, invalid_col2 = 0;
+		string expected="0 0 0 \n";
+		Matrix b = Matrix(invalid_row2,invalid_col2);
+
+		ASSERT_EQUAL(expected,b.toString());
+
+	}
+
+void testConstructor1Valid() {
+    int valid_row = 3, valid_col = 4;
+		// test constructor1; valid inputs
+    string row1="0 0 0 0 \n", row2="0 0 0 0 \n",row3="0 0 0 0 \n";
+    string expected=row1+row2+row3;
+	Matrix d = Matrix(valid_row,valid_col);
+
+        ASSERT_EQUAL(expected,d.toString());
+
+	}
+
+//new testcase for Matrix(int, int) - Constructor 1
+void testConstructor1ValidTest() {
+    int valid_row = 2, valid_col = 2;
+		// test constructor1; valid inputs
+    string row1="0 0 \n", row2="0 0 \n";
+    string expected=row1+row2;
+	Matrix d = Matrix(valid_row,valid_col);
+
+        ASSERT_EQUAL(expected,d.toString());
+
+	}
+
+void testConstructor2() {
+	  // for constructor2
+	    int row=4,col=5;
+        int data[4][5] = {{1,2,3,4,5},{6,7,8,9,0},{0,0,1,2,3},{0,0,0,4,5}};
+        int*input_data[4];
+        for (int i=0;i<4;i++){
+        	input_data[i]=data[i];
+        }
+		Matrix f =  Matrix(row,col,input_data);
+
+	    string row1="1 2 3 4 5 \n", row2="6 7 8 9 0 \n", row3="0 0 1 2 3 \n",row4="0 0 0 4 5 \n";
+	    string expected=row1+row2+row3+row4;
+        ASSERT_EQUAL(expected,f.toString());
+
+}
+
+//Constructor 2 test - Table Constructor
+void testConstructor2Test() {
+	  // for constructor2
+	    int row=3,col=2;
+        int data[3][2] = {{1,4},{6,8},{0,0}};
+        int*input_data[3];
+        for (int i=0;i<3;i++){
+        	input_data[i]=data[i];
+        }
+		Matrix f =  Matrix(row,col,input_data);
+
+	    string row1="1 4 \n", row2="6 8 \n", row3="0 0 \n";
+	    string expected=row1+row2+row3;
+        ASSERT_EQUAL(expected,f.toString());
+
+}
+
+void testGetElementValid() {
+	  // for constructor2
+    int row=4,col=5;
+    int data[4][5] = {{1,2,3,4,5},{6,7,8,9,0},{0,0,1,2,3},{0,0,0,4,5}};
+    int*input_data[4];
+    for (int i=0;i<4;i++){
+    	input_data[i]=data[i];
+    }
+	Matrix f =  Matrix(row,col,input_data);
+
+		int expected= 9;
+        ASSERT_EQUAL(expected,f.getElement(1, 3));
+
+	}
+
+
+void testGetElementInvalid() {
+	  // for constructor2
+    int row=4,col=5;
+    int data[4][5] = {{1,2,3,4,5},{6,7,8,9,0},{0,0,1,2,3},{0,0,0,4,5}};
+    int*input_data[4];
+    for (int i=0;i<4;i++){
+    	input_data[i]=data[i];
+    }
+	Matrix f =  Matrix(row,col,input_data);
+	string actual ="Empty for Now";
+	try {
+			int output = f.getElement(10, 2);	// invalid indexes
+	}
+	catch (std::out_of_range& e){
+			 actual = e.what();
+	}
+
+    string expected= "Invalid indexes.";
+    ASSERT_EQUAL(expected,actual);
+
+}
+
+//New test case for getElement
+void testGetElementValidTest() {
+	  // for constructor2
+    int row=3,col=2;
+    int data[3][2] = {{1,4},{6,8},{0,0}};
+    int*input_data[3];
+    for (int i=0;i<3;i++){
+        input_data[i]=data[i];
+    }
+	Matrix f =  Matrix(row,col,input_data);
+
+		int expected= 6;
+        ASSERT_EQUAL(expected,f.getElement(1, 0));
+
+	}
+
+
+void testSetElementValid() {
+	  int value = 100;
+	  int setvalid_row = 0, setvalid_col = 0;
+	    int row=4,col=5;
+	    int data[4][5] = {{1,2,3,4,5},{6,7,8,9,0},{0,0,1,2,3},{0,0,0,4,5}};
+	    int*input_data[4];
+	    for (int i=0;i<4;i++){
+	    	input_data[i]=data[i];
+	    }
+		Matrix f =  Matrix(row,col,input_data);
+	  bool check = f.setElement(value,setvalid_row,setvalid_col);
+	  string row1="100 2 3 4 5 \n", row2="6 7 8 9 0 \n", row3="0 0 1 2 3 \n",row4="0 0 0 4 5 \n";
+	  string expected=row1+row2+row3+row4;
+
+      ASSERT_EQUAL(expected,f.toString());
+      ASSERT_EQUAL(true,check);
+
+	}
+
+
+
+void testSetElementInvalid() {
+	  int value = 100;
+	  int setinvalid_row = 10, setinvalid_col = 10;
+	  int row=4,col=5;
+      int data[4][5] = {{1,2,3,4,5},{6,7,8,9,0},{0,0,1,2,3},{0,0,0,4,5}};
+	  int*input_data[4];
+	    for (int i=0;i<4;i++){
+	    	input_data[i]=data[i];
+	    }
+	  Matrix f =  Matrix(row,col,input_data);
+	  bool check = f.setElement(value,setinvalid_row,setinvalid_col);
+	  string row1="1 2 3 4 5 \n", row2="6 7 8 9 0 \n", row3="0 0 1 2 3 \n",row4="0 0 0 4 5 \n";
+	  string expected=row1+row2+row3+row4;
+      ASSERT_EQUAL(expected,f.toString());
+      ASSERT_EQUAL(false,check);
+
+}
+
+//New test case for set
+void testSetElementValidTest() {
+	  int value = 100;
+	  int setvalid_row = 0, setvalid_col = 0;
+	  int row=3,col=2;
+      int data[3][2] = {{1,4},{6,8},{0,0}};
+      int*input_data[3];
+      for (int i=0;i<3;i++){
+        input_data[i]=data[i];
+      }
+		Matrix f =  Matrix(row,col,input_data);
+	  bool check = f.setElement(value,setvalid_row,setvalid_col);
+	  string row1="100 4 \n", row2="6 8 \n", row3="0 0 \n";
+	  string expected=row1+row2+row3;
+
+      ASSERT_EQUAL(expected,f.toString());
+      ASSERT_EQUAL(true,check);
+
+	}
+
+
+
+void testCopy() {
+
+	int row=4,col=5;
+    int data[4][5] = {{1,2,3,4,5},{6,7,8,9,0},{0,0,1,2,3},{0,0,0,4,5}};
+	int*input_data[4];
+	for (int i=0;i<4;i++){
+	    	input_data[i]=data[i];
+	}
+	Matrix origin =  Matrix(row,col,input_data);
+    Matrix copied = origin.copy(); // make a copy
+	string row1="1 2 3 4 5 \n", row2="6 7 8 9 0 \n", row3="0 0 1 2 3 \n",row4="0 0 0 4 5 \n";
+	string expected=row1+row2+row3+row4;
+    ASSERT_EQUAL(expected,copied.toString());
+}
+
+//New Test case
+void testCopyTest() {
+
+	int row=3,col=2;
+    int data[3][2] = {{1,4},{6,8},{0,0}};
+    int*input_data[3];
+    for (int i=0;i<3;i++){
+        input_data[i]=data[i];
+    }
+	Matrix origin =  Matrix(row,col,input_data);
+    Matrix copied = origin.copy(); // make a copy
+	string row1="1 4 \n", row2="6 8 \n", row3="0 0 \n";
+	string expected=row1+row2+row3;
+    ASSERT_EQUAL(expected,copied.toString());
+}
+
+void testAddToInvalid() {
+
+
+
+	   /*Add-to Matrix*/
+	    int row_to_add=4; int col_to_add=3;
+
+
+		int add_to_data[4][3] = {{3,4,5}, {1,2,3}, {0,0,0},{1,2,1}}; // add to 'data' matrix, dimension not match
+
+	 	int*add_input_data[4];
+	 	for (int i=0;i<4;i++){
+	 	    	add_input_data[i]=add_to_data[i];
+	 	}
+
+	 	/*Original Matrix*/
+	    int row=4;int col=5;
+	    int data[4][5] = {{1,2,3,4,5},{6,7,8,9,0},{0,0,1,2,3},{0,0,0,4,5}};
+	 	int*input_data[4];
+	 	for (int i=0;i<4;i++){
+	 	    	input_data[i]=data[i];
+	 	}
+	 	Matrix origin =  Matrix(row,col,input_data);
+
+		string actual ="Empty for Now";
+		string expected = "Invalid operation.";
+        Matrix m = Matrix(row_to_add,col_to_add,add_input_data);
+
+
+        try{
+          m.addTo(origin);
+
+        }
+        catch (const std::invalid_argument& e) {
+    	   actual = e.what();
+        }
+        ASSERT_EQUAL(expected,actual);
+}
+
+
+void testAddToValid() {
+	   int add_to_data[4][3] = {{3,4,5}, {1,2,3}, {0,0,0},{1,2,1}}; // add to 'data' matrix, dimension not match
+	   int add_to_above[4][3] = {{1,0,-1}, {2,1,0}, {2,2,2},{0,-1,0}}; // add to 'add_to_data' matrix
+	   int row=4;int col=3;
+
+	   int*input_data[4];
+	   for (int i=0;i<4;i++){
+	 	    	input_data[i]=add_to_data[i];
+	   }
+	   Matrix m =  Matrix(row,col,input_data);
+
+	   for (int i=0;i<4;i++){
+	 	    	input_data[i]=add_to_above[i];
+	   }
+	   Matrix m1 =  Matrix(row,col,input_data);
+	   string row1="4 4 4 \n", row2="3 3 3 \n", row3="2 2 2 \n",row4="1 1 1 \n";
+       string expected = row1+row2+row3+row4;
+
+
+
+      try{
+        m.addTo(m1);
+        ASSERT_EQUAL(expected,m.toString());
+
+      }
+    catch (const std::invalid_argument& e) {
+	   std::cerr << "supposed to be a valid input: " << e.what() << '\n';
+    }
+
+}
+
+
+//Test Case for addTo()
+
+void testAddToValidTest() {
+	   int add_to_data[4][3] = {{2,3,4}, {1,2,3}, {1,2,3},{0,0,0}}; // add to 'data' matrix, dimension not match
+	   int add_to_above[4][3] = {{1,0,-1}, {3,0,-3}, {4,5,-6},{0,0,0}}; // add to 'add_to_data' matrix
+	   int row=4;int col=3;
+
+	   int*input_data[4];
+	   for (int i=0;i<4;i++){
+	 	    	input_data[i]=add_to_data[i];
+	   }
+	   Matrix m =  Matrix(row,col,input_data);
+
+	   for (int i=0;i<4;i++){
+	 	    	input_data[i]=add_to_above[i];
+	   }
+	   Matrix m1 =  Matrix(row,col,input_data);
+	   string row1="3 3 3 \n", row2="4 2 0 \n", row3="5 7 -3 \n",row4="0 0 0 \n";
+       string expected = row1+row2+row3+row4;
+
+
+
+      try{
+        m.addTo(m1);
+        ASSERT_EQUAL(expected,m.toString());
+
+      }
+    catch (const std::invalid_argument& e) {
+	   std::cerr << "supposed to be a valid input: " << e.what() << '\n';
+    }
+
+}
+
+
+
+
+
+
+void testSubMatrixValid() {
+    int subvalid_row = 3, subvalid_col = 3;
+
+
+	  int row=4,col=5;
+      int data[4][5] = {{1,2,3,4,5},{6,7,8,9,0},{0,0,1,2,3},{0,0,0,4,5}};
+	  int*input_data[4];
+	  for (int i=0;i<4;i++){
+	    	input_data[i]=data[i];
+	  }
+	  Matrix f =  Matrix(row,col,input_data);
+
+		string row1="1 2 3 4 \n", row2="6 7 8 9 \n", row3="0 0 1 2 \n",row4="0 0 0 4 \n";
+		string expected=row1+row2+row3+row4;
+
+		try{
+			Matrix y = f.subMatrix(subvalid_row,subvalid_col);
+			ASSERT_EQUAL(expected,y.toString());
+		}
+		catch (std::out_of_range& e){
+			std::cerr << "supposed to be a valid input: " << e.what() << '\n';
+		}
+
+
+}
+
+
+
+void testSubMatrixInvalid() {
+
+    int subinvalid_row = 3, subinvalid_col = 6;
+
+	  int row=4,col=5;
+      int data[4][5] = {{1,2,3,4,5},{6,7,8,9,0},{0,0,1,2,3},{0,0,0,4,5}};
+	  int*input_data[4];
+	  for (int i=0;i<4;i++){
+	    	input_data[i]=data[i];
+	  }
+	  Matrix f =  Matrix(row,col,input_data);
+
+		string actual ="Empty for Now";
+		string expected = "Submatrix not defined.";
+
+		try{
+			Matrix y = f.subMatrix(subinvalid_row,subinvalid_col);
+		}
+		catch (std::out_of_range& e){
+
+			actual = e.what();
+		}
+		ASSERT_EQUAL(expected,actual);
+
+
+}
+
+//Test case for SubMatrix
+void testSubMatrixValidTest() {
+    int subvalid_row = 3, subvalid_col = 3;
+
+
+	  int row=4,col=5;
+      int data[4][5] = {{4,2,33,54,-5},{5,7,18,79,30},{3,7,3,3,3},{10,110,110,14,15}};
+	  int*input_data[4];
+	  for (int i=0;i<4;i++){
+	    	input_data[i]=data[i];
+	  }
+	  Matrix f =  Matrix(row,col,input_data);
+
+		string row1="4 2 33 54 \n", row2="5 7 18 79 \n", row3="3 7 3 3 \n",row4="10 110 110 14 \n";
+		string expected=row1+row2+row3+row4;
+
+		try{
+			Matrix y = f.subMatrix(subvalid_row,subvalid_col);
+			ASSERT_EQUAL(expected,y.toString());
+		}
+		catch (std::out_of_range& e){
+			std::cerr << "supposed to be a valid input: " << e.what() << '\n';
+		}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+bool runAllTests(int argc, char const *argv[]) {
+	cute::suite s;
+    s.push_back(CUTE(testConstructor1Invalid1));
+
+	s.push_back(CUTE(testConstructor1Invalid2));
+
+	s.push_back(CUTE(testConstructor1Valid));
+
+	//new Testcase for first constructor
+	s.push_back(CUTE(testConstructor1ValidTest));
+
+
+
+	s.push_back(CUTE(testConstructor2));
+
+	//Constructor 2 new testcase:
+	s.push_back(CUTE(testConstructor2Test));
+
+
+	s.push_back(CUTE(testGetElementValid));
+	s.push_back(CUTE(testGetElementInvalid));
+
+	//Get new Test case:
+	s.push_back(CUTE(testGetElementValidTest));
+
+	s.push_back(CUTE(testSetElementValid));
+	s.push_back(CUTE(testSetElementInvalid));
+
+	//set new test cases:
+	s.push_back(CUTE(testSetElementValidTest));
+
+	s.push_back(CUTE(testCopy));
+	
+
+	//NewTest Case for copy
+	s.push_back(CUTE(testCopyTest));
+
+	s.push_back(CUTE(testAddToInvalid));
+	s.push_back(CUTE(testAddToValid));
+
+	//test case for addTo
+	s.push_back(CUTE(testAddToValidTest));
+
+
+	s.push_back(CUTE(testSubMatrixValid));
+	s.push_back(CUTE(testSubMatrixInvalid));
+
+	//test case for subMatrix
+	s.push_back(CUTE(testSubMatrixValidTest));
+
+
+
+	//TODO add your test here
+
+	cute::xml_file_opener xmlfile(argc, argv);
+	cute::xml_listener<cute::ide_listener<> > lis(xmlfile.out);
+	auto runner = cute::makeRunner(lis, argc, argv);
+	bool success = runner(s, "AllTests");
+	return success;
+}
+
+int main(int argc, char const *argv[]) {
+    return runAllTests(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
